72122e711a72510511e06929596b945be17f1172
pdmurray <peynmurray@gmail.com>
2024-07-01 14:08:52 -0700

    Create benchmarks branch

--- versioned_hdf5/__init__.py
+++
@@ -1,8 +0,0 @@
-from importlib.metadata import version
-
-from .api import VersionedHDF5File
-from .replay import delete_version, delete_versions, modify_metadata
-
-__version__ = version(__package__)
-
-__all__ = ["VersionedHDF5File", "delete_version", "delete_versions", "modify_metadata"]
