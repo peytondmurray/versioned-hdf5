2e8b6fb595f9ba5a1c6171cfa00dbc51dbad05a3
pdmurray <peynmurray@gmail.com>
2024-07-02 11:16:07 -0700

    Switch to local benchmarking

---
+++ benchmarks_update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git fetch origin
+git fetch --tags
+git checkout master
+git checkout benchmarks -- .asv/ 2>/dev/null || echo ".asv/ doesn't exist in the benchmarks branch yet."
+
+pip install '.[bench]'
+
+asv machine --yes
+for ref in $(git tag) $(git rev-list 1.7.0..HEAD); do
+    asv run --skip-existing --show-stderr $ref
+done
+
+asv show
+
+git checkout benchmarks
+git add -f .asv/
+git commit -m "Updated benchmarks"
+git push origin benchmarks
+git checkout master
