cdbf0e46ff3b556834fbefc71d829127f0254802
pdmurray <peynmurray@gmail.com>
2024-08-09 14:44:54 -0700

    Continue trying to implement cpp slicetools

--- versioned_hdf5/slices.cpp
+++ versioned_hdf5/slices.cpp
@@ -3,18 +3,84 @@
 
 namespace py = pybind11;
 
+py::tuple spaceid_to_slice(hid_t id) {
+
+    int rank;
+    std::vector<hsize_t> start_array;
+    std::vector<hsize_t> stride_array;
+    std::vector<hsize_t> count_array;
+    std::vector<hsize_t> block_array;
+    htri_t ret;
+    py::object slices = py::list();
+
+    py::object Slice = py::module_::import("ndindex");
+
+    switch (H5Sget_select_type(id)) {
+        case H5S_SEL_ALL:
+            return py::tuple();
+        case H5S_SEL_NONE:
+            return py::make_tuple(0, 0);
+        case H5S_SEL_HYPERSLABS:
+
+            rank = H5Sget_simple_extent_ndims(id);
+            if (rank < 0) {
+                throw py::value_error("Cannot determine rank of selection.");
+            }
+
+            ret = H5Sget_regular_hyperslab(
+                id,
+                start_array.data(),
+                stride_array.data(),
+                count_array.data(),
+                block_array.data()
+            );
+
+            if (ret < 0) {
+                throw py::value_error("Cannot determine hyperslab selection.");
+            }
+
+            for (int i=0; i<rank; i++) {
+                hsize_t start = start_array[i];
+                hsize_t stride = stride_array[i];
+                hsize_t count = count_array[i];
+                hsize_t block = block_array[i];
+
+
+                if (block != 1 && count != 1) {
+                    throw py::value_error("Nontrivial blocks are not yet supported.");
+                }
+
+                hsize_t end = start + (stride*(count - 1) + 1)*block;
+                if (block != 1) {
+                    stride = 1;
+                }
+
+                slices.attr("append")(Slice(start, end, stride));
+            }
+
+            return slices;
+        default:
+            throw py::value_error("Point selections are not yet supported");
+    }
+}
+
+
 py::dict build_data_dict(py::object dcpl, py::str raw_data_name) {
-    auto data_dict = py::dict();
+    py::dict data_dict = py::dict();
 
     hid_t dcpl_id = py::cast<hid_t>(dcpl.attr("id"));
     size_t virtual_count = py::cast<size_t>(dcpl.attr("get_virtual_count")());
-    char* dname = py::cast<char *>(raw_data_name);
+
+    char *dname;
+    dname = py::cast<char *>(raw_data_name);
     size_t dnamelen = py::len(raw_data_name) + 1;
 
     char *vfilename = (char *)malloc(2);
     char *dset_buf = (char *)malloc(dnamelen);
 
-    for (int j=0; j<virtual_count; j++) {
+    py::object spaceid_to_slice = (py::object) py::module_::import("slicetools").attr("spaceid_to_slice");
+
+    for (size_t j=0; j<virtual_count; j++) {
 
         // Check that the virtual file is '.'
         if (H5Pget_virtual_filename(dcpl_id, j, vfilename, 2) < 0) {
